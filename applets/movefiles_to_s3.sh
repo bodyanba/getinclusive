b2904697662e80f9da313e765187c59fb35f4921
bodyanba <galkinbogdan@gmail.com>
2020-02-13 13:13:32 +0300

    1 commit

---
+++ applets/movefiles_to_s3.sh
@@ -0,0 +1,44 @@
+aws s3 cp 'Alcohol_Drunk Friend_Dialogue_V1.3' 's3://getinclusive-assets/Hype/voices_for_change/Alcohol_Drunk Friend_Dialogue_V1.3' --recursive
+aws s3 cp 'Alcohol_Smart Partying_Infographic_V1.0' 's3://getinclusive-assets/Hype/voices_for_change/Alcohol_Smart Partying_Infographic_V1.0' --recursive
+aws s3 cp 'Alcohol_Smart Partying_Infographic_V1.0_Barton CC' 's3://getinclusive-assets/Hype/voices_for_change/Alcohol_Smart Partying_Infographic_V1.0_Barton CC' --recursive
+aws s3 cp 'Alcohol_Smart Partying_Infographic_V1.0_Wayne State' 's3://getinclusive-assets/Hype/voices_for_change/Alcohol_Smart Partying_Infographic_V1.0_Wayne State' --recursive
+aws s3 cp 'Alcohol_Standard Drink_Flipcard Quiz_V1.6' 's3://getinclusive-assets/Hype/voices_for_change/Alcohol_Standard Drink_Flipcard Quiz_V1.6' --recursive
+aws s3 cp 'Alcohol_The Data_Infographic_V1.0' 's3://getinclusive-assets/Hype/voices_for_change/Alcohol_The Data_Infographic_V1.0' --recursive
+aws s3 cp 'Bullying By The Numbers' 's3://getinclusive-assets/Hype/groundswell/Bullying By The Numbers' --recursive
+aws s3 cp 'Business As Usual' 's3://getinclusive-assets/Hype/groundswell/Business As Usual' --recursive
+aws s3 cp 'CA_CaseStudy_Lydia' 's3://getinclusive-assets/Hype/groundswell/CA_CaseStudy_Lydia' --recursive
+aws s3 cp 'Case Study_Cole' 's3://getinclusive-assets/Hype/groundswell/Case Study_Cole' --recursive
+aws s3 cp 'Compliments' 's3://getinclusive-assets/Hype/groundswell/Compliments' --recursive
+aws s3 cp 'Creating Inclusion' 's3://getinclusive-assets/Hype/tapestry/Creating Inclusion' --recursive
+aws s3 cp 'Crossroads' 's3://getinclusive-assets/Hype/groundswell/Crossroads' --recursive
+aws s3 cp 'FontTester' 's3://getinclusive-assets/Hype/testing/FontTester' --recursive
+aws s3 cp 'Groundswell Preview Landing Page' 's3://getinclusive-assets/Hype/testing/Groundswell Preview Landing Page' --recursive
+aws s3 cp 'Hazing_The Data_Infographic_V1.0' 's3://getinclusive-assets/Hype/voices_for_change/Hazing_The Data_Infographic_V1.0' --recursive
+aws s3 cp 'Hazing_The Data_Infographic_V1.0_Wayne State' 's3://getinclusive-assets/Hype/voices_for_change/Hazing_The Data_Infographic_V1.0_Wayne State' --recursive
+aws s3 cp 'How We Party' 's3://getinclusive-assets/Hype/voices_for_change/How We Party' --recursive
+aws s3 cp 'Imagine Scenario' 's3://getinclusive-assets/Hype/groundswell/Imagine Scenario' --recursive
+aws s3 cp 'In Reverse' 's3://getinclusive-assets/Hype/groundswell/In Reverse' --recursive
+aws s3 cp 'Joanie' 's3://getinclusive-assets/Hype/groundswell/Joanie' --recursive
+aws s3 cp 'Jokes' 's3://getinclusive-assets/Hype/groundswell/Jokes' --recursive
+aws s3 cp 'LMS Pages' 's3://getinclusive-assets/Hype/testing/LMS Pages' --recursive
+aws s3 cp 'Leia' 's3://getinclusive-assets/Hype/groundswell/Leia' --recursive
+aws s3 cp 'Luther Jocelyn' 's3://getinclusive-assets/Hype/groundswell/Luther Jocelyn' --recursive
+aws s3 cp 'Maybe They Didn't See' 's3://getinclusive-assets/Hype/groundswell/Maybe They Didn't See' --recursive
+aws s3 cp 'One of Them' 's3://getinclusive-assets/Hype/voices_for_change/One of Them' --recursive
+aws s3 cp 'Our Biases' 's3://getinclusive-assets/Hype/voices_for_change/Our Biases' --recursive
+aws s3 cp 'Quiz Question_QPQ' 's3://getinclusive-assets/Hype/testing/Quiz Question_QPQ' --recursive
+aws s3 cp 'Romantic' 's3://getinclusive-assets/Hype/groundswell/Romantic' --recursive
+aws s3 cp 'STOP Infographic' 's3://getinclusive-assets/Hype/voices_for_change/STOP Infographic' --recursive
+aws s3 cp 'Samuel' 's3://getinclusive-assets/Hype/groundswell/Samuel' --recursive
+aws s3 cp 'Sex Gender Continuums' 's3://getinclusive-assets/Hype/groundswell/Sex Gender Continuums' --recursive
+aws s3 cp 'Sexual Violence_Friends Dilemma_Dialogue_V1.1' 's3://getinclusive-assets/Hype/voices_for_change/Sexual Violence_Friends Dilemma_Dialogue_V1.1' --recursive
+aws s3 cp 'Sexual Violence_The Data_Infographic_V1.0' 's3://getinclusive-assets/Hype/voices_for_change/Sexual Violence_The Data_Infographic_V1.0' --recursive
+aws s3 cp 'Sexual Violence_The Data_Infographic_V1.0_Moody' 's3://getinclusive-assets/Hype/voices_for_change/Sexual Violence_The Data_Infographic_V1.0_Moody' --recursive
+aws s3 cp 'Sexual Violence_The Data_Infographic_V1.0_Wayne State' 's3://getinclusive-assets/Hype/voices_for_change/Sexual Violence_The Data_Infographic_V1.0_Wayne State' --recursive
+aws s3 cp 'SkillGarden' 's3://getinclusive-assets/Hype/testing/SkillGarden' --recursive
+aws s3 cp 'Study Time' 's3://getinclusive-assets/Hype/voices_for_change/Study Time' --recursive
+aws s3 cp 'That's Not Okay' 's3://getinclusive-assets/Hype/groundswell/That's Not Okay' --recursive
+aws s3 cp 'Third Party' 's3://getinclusive-assets/Hype/groundswell/Third Party' --recursive
+aws s3 cp 'Why Does it Matter' 's3://getinclusive-assets/Hype/groundswell/Why Does it Matter' --recursive
+aws s3 cp 'applet2.v11' 's3://getinclusive-assets/Hype/testing/applet2.v11' --recursive
+aws s3 cp 'wpress' 's3://getinclusive-assets/Hype/testing/wpress' --recursive
---
+++ js/common.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+	$('[data-toggle="tooltip"]').tooltip().click(function(e) {
+		e.stopPropagation();
+		$('[data-toggle="tooltip"]').not(this).tooltip('hide');
+		$(this).tooltip('toggle');
+	});
+
+	$(document).click(function(e) {
+		var id = $(e.target).closest(".tooltip").attr("id");
+		var attr = "[aria-describedby=" + "'" + id + "'" + "]";
+		if (!$(e.target).is('.tooltip *'))
+			$('[data-toggle="tooltip"]').tooltip('hide');
+		else
+			$('#link-clock' + attr).tooltip('hide');
+		if ($(e.target).is('.tooltip .button-no'))
+			$(attr).tooltip('hide');
+	});
+
+	$(document).on('click.bs.dropdown.data-api', '.dropdown-menu', function (e) { 
+		e.stopPropagation();
+	});
+
+	function resetForm(){
+		$(".dropdown-menu").removeClass('active');
+		// $("input").prop( "checked", false);
+		// $("textarea").val("");
+	};
+
+	function btnSubmit() { 
+		var value;
+		if ($("input").is(":checked") && $("textarea").val())
+			value = false
+		else 
+			value = true
+		$("[name='submit']").prop( "disabled", value);
+	};
+
+	$('.dropdown-cancel').on('click', function (e) { 
+		$("[data-toggle='dropdown']").dropdown('toggle');
+		resetForm();
+		btnSubmit();
+	});
+	$('.dropdown').on('hide.bs.dropdown', function () {
+		resetForm();
+		btnSubmit();
+	});
+	$('.dropdown').on('show.bs.dropdown', function () {
+		$('[data-toggle="tooltip"]').tooltip('hide');
+	});
+
+	$('[name="submit"]').on('click', function (e) {
+		e.stopPropagation();
+		$(".dropdown-menu").addClass('active');
+	});
+
+	$("input, textarea").on("keyup change", function () {
+		btnSubmit();
+	});
+
+});
